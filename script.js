63dd4feed9328af58c1269068f87fc672d099ca0
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-03-29 22:58:13 -0300

    Colocando mão na massa com no JS

---
+++ script.js
@@ -0,0 +1,33 @@
+// Initial Data
+let currentQuestion = 0;
+let correctAnswers = 0;
+showQuestion();
+
+// Events
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
+// Functions
+function showQuestion() {
+    if(questions[currentQuestion]) {
+        let q = questions[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        });
+    } else {
+        finishQuiz();
+    }
+}
