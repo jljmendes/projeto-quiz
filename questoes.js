d9052e10ba9663041901b529ef5723c1f8c894bf
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-04-01 00:17:42 -0300

    Finalizando as questão

--- questoes.js
+++ questoes.js
@@ -35,5 +35,71 @@ let questions = [
         ],
         answer: 1
     },
+    {
+        question: 'Como escrever um "Olá Mundo" em um alertbox?',
+        options: [
+            'alertBox("Olá Mundo")',
+            'msgBox("Olá Mundo")',
+            'msg("Olá Mundo")',
+            'alert("Olá Mundo")'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Como você cria uma função no Javascript?',
+        options: [
+            'function:minhaFuncao()',
+            'function = minhaFuncao()',
+            'function minhaFuncao()'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Como chamar uma função chamada "minhaFuncao"?',
+        options: [
+            'call minhaFuncao',
+            'call function minhaFuncao',
+            'minhaFuncao()'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Como escrever uma condicional IF no Javascript?',
+        options: [
+            'if i = 5',
+            'if i == 5 then',
+            'if (i == 5)',
+            'if i = 5 then'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Como fazer um if que executa um código caso "i" for diferente de 5',
+        options: [
+            'if (i != 5)',
+            'if (i <> 5)',
+            'if i <> 5',
+            'if i =! 5 then'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Como o loop while começa?',
+        options: [
+            'while (i <= 10)',
+            'while (i <= 10; i++)',
+            'while i = 1 to 10'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Gostou do projeto?',
+        options: [
+            'Sim',
+            'Não',
+            'Talvez'
+        ],
+        answer: 0
+    },
 
 ];
