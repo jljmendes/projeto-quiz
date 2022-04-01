7e103438f2a76ed86b0d48114339033dc26e58a0
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-03-31 21:28:57 -0300

    Adicionando a primeira questão

---
+++ questoes.js
@@ -0,0 +1,12 @@
+let questions = [
+    {
+        question: 'Em qual elemento HTML nós colocamos o javascript?',
+        options: [
+            'tag js',
+            'tag script',
+            'tag scripting',
+            'tag javascript'
+        ],
+        answer: 1
+    },
+];
